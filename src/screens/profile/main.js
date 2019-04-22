99d27017632b5f5dc13ea3689ac6bbcafc9ddaad
Den Abramov <denys.abramov@gliesereum.com>
2019-04-22 17:47:09 +0300

    Profile update: Added handler empty field;

--- src/screens/profile/main.js
+++ src/screens/profile/main.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, {Component} from "react";
+import {connect} from "react-redux";
 import {
   Text,
   Container,
@@ -14,34 +14,28 @@ import {
   Picker
 } from "native-base";
 
-const deviceWidth = Dimensions.get("window").width;
-
-
-import { asyncRequestTest } from "../../utils";
-
-import { HeaderLayout } from "../../components/Layout";
-
+import {asyncRequestTest} from "../../utils";
+import {HeaderLayout} from "../../components/Layout";
 import authActions from "../../redux/auth/actions";
 import appActions from "../../redux/app/actions";
-import { Dimensions } from "react-native";
 
 const fields = [
-  { label: "Имя", key: "firstName", render: true },
-  { label: "Фамилия", key: "lastName", render: true },
-  { label: "Отчество", key: "middleName", render: true },
-  { label: "Должность", key: "position", render: true },
-  { label: "Страна Проживания", key: "country", render: true },
-  { label: "Город Проживания", key: "city", render: true },
-  { label: "Адресс, 8 символов", key: "address", render: true },
-  { label: "Дополнительный Адресс, 8 символов", key: "addAddress", render: true },
-  { label: "Аватар (URL)", key: "avatarUrl", render: false, defaultValue: "http" },
-  { label: "Обложка (URL)", key: "coverUrl", render: false, defaultValue: "http" },
+  {label: "Имя", key: "firstName", render: true, defaultValue: 'Не заполнено'},
+  {label: "Фамилия", key: "lastName", render: true, defaultValue: 'Не заполнено'},
+  {label: "Отчество", key: "middleName", render: true, defaultValue: 'Не заполнено'},
+  {label: "Должность", key: "position", render: true, defaultValue: 'Не заполнено'},
+  {label: "Страна Проживания", key: "country", render: true, defaultValue: 'Не заполнено'},
+  {label: "Город Проживания", key: "city", render: true, defaultValue: 'Не заполнено'},
+  {label: "Адресс, 8 символов", key: "address", render: true, defaultValue: 'Не заполнено'},
+  {label: "Дополнительный Адресс, 8 символов", key: "addAddress", render: false, defaultValue: 'Не заполнено'},
+  {label: "Аватар (URL)", key: "avatarUrl", render: false, defaultValue: null},
+  {label: "Обложка (URL)", key: "coverUrl", render: false, defaultValue: null},
   {
     label: "Пол", key: "gender", type: "select", options: [
-      { label: "Не указан", key: "UNKNOWN" },
-      { label: "Мужской", key: "MALE" },
-      { label: "Женский", key: "FEMALE" }
-    ]
+      {label: "Не указан", key: "UNKNOWN"},
+      {label: "Мужской", key: "MALE"},
+      {label: "Женский", key: "FEMALE"}
+    ], defaultValue: 'UNKNOWN'
   }
 ];
 
@@ -49,7 +43,7 @@ const fields = [
 class Main extends Component {
 
   state = {
-    data: { avatarUrl: "http", coverUrl: "http" }
+    data: {}
   };
 
   componentWillMount() {
@@ -57,49 +51,58 @@ class Main extends Component {
   }
 
   initForm = () => {
-    const { user } = this.props.auth;
+    const {user} = this.props.auth;
+    this.setState(state => ({...state, data: user}));
+  };
 
-    this.setState(state => ({ ...state, data: user }));
+  fillUserData = obj => {
+    return fields.reduce((acc, field) => {
+      if (!obj[field.key] || !obj[field.key].length) {
+        obj[field.key] = field.defaultValue;
+        return obj
+      }
+      return obj
+    }, obj)
   };
 
   onInput = (key, value) => {
-    this.setState(state => ({ ...state, data: { ...state.data, [key]: value } }));
+    this.setState(state => ({...state, data: {...state.data, [key]: value}}));
   };
 
   updateProfile = async () => {
-    const { $updateUser, $globalSpinnerOn, $globalSpinnerOff, navigation } = this.props;
+    const {$updateUser, $globalSpinnerOn, $globalSpinnerOff, navigation} = this.props;
     await $globalSpinnerOn();
     try {
       const url = `user`;
-      const { data } = this.state;
-      const { token } = this.props.auth;
-      const newUser = await asyncRequestTest(url, "PUT", "account", token, data);
+      const {data} = this.state;
+      const {token} = this.props.auth;
+      const newUser = await asyncRequestTest(url, "PUT", "account", token, this.fillUserData(data));
       await $updateUser(newUser);
-      Toast.show({ text: "Успешно обновлён профиль" });
+      Toast.show({text: "Успешно обновлён профиль"});
       navigation.goBack();
     } catch (e) {
-      Toast.show({ text: e.message || "Ошибка" });
+      Toast.show({text: 'Заполните все поля!'});
     } finally {
       await $globalSpinnerOff();
     }
   };
 
   renderItemInput = (item) => {
-    const { onInput } = this;
+    const {onInput} = this;
     const value = this.state.data[item.key];
     const genders = [
-      { label: "Не указан", key: "UNKNOWN" },
-      { label: "Мужской", key: "MALE" },
-      { label: "Женский", key: "FEMALE" }
+      {label: "Не указан", key: "UNKNOWN"},
+      {label: "Мужской", key: "MALE"},
+      {label: "Женский", key: "FEMALE"}
     ];
     if (item.type === "select") {
       return (
 
         <Item key={item.key}>
-          <Label style={{ paddingTop: 4 }}>{item.label}</Label>
+          <Label style={{paddingTop: 4}}>{item.label}</Label>
           <Picker
             iosIcon={<Icon name="ios-arrow-down"/>}
-            style={{ width: "100%", marginTop: 3 }}
+            style={{width: "100%", marginTop: 3}}
             key={item.key}
             mode="dropdown"
             iosHeader={item.label}
@@ -120,7 +123,7 @@ class Main extends Component {
     }
     return item.render && (
       <Item fixedLabel key={item.key}>
-        <Label style={{ paddingTop: 4 }}>{item.label}</Label>
+        <Label style={{paddingTop: 4}}>{item.label}</Label>
         <Input value={value} onChangeText={text => onInput(item.key, text)}/>
       </Item>
     );
@@ -141,13 +144,14 @@ class Main extends Component {
           <Form>
             {fields.map(this.renderItemInput)}
           </Form>
-          <Button block style={{ margin: 15, marginTop: 16 }} onPress={this.updateProfile}>
+          <Button block style={{margin: 15, marginTop: 16}} onPress={this.updateProfile}>
             <Text>Сохранить</Text>
           </Button>
         </Content>
       </Container>
     );
   }
+
 }
 
-export default connect(state => state, ({ ...authActions, ...appActions }))(Main);
+export default connect(state => state, ({...authActions, ...appActions}))(Main);
